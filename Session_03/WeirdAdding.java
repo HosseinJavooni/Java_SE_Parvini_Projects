f623822184c4de5f35b771382e068d0185693c54
HosseinJavooni <h.javooni@gmail.com>
2023-05-08 15:10:39 +0330

    1402-02-18_01

---
+++ Session_03/WeirdAdding.java
@@ -0,0 +1,10 @@
+public class WeirdAdding{
+	public static void main(String[] args){
+		double a = 0.7;
+		double c = 0.1;
+		double b = a + c;
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(c);
+	}
+}
