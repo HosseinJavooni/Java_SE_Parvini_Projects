aca5cb9e760560254b150ab6439ba094844708f0
HosseinJavooni <h.javooni@gmail.com>
2023-05-20 00:35:57 +0330

    1402_02_29

---
+++ Session_05/src/main/java/StringStringBufferStringBuilder.java
@@ -0,0 +1,16 @@
+import org.openjdk.jol.vm.VM;
+
+public class StringStringBufferStringBuilder {
+
+    public static void main(String[] args){
+        String s = "Hossein";
+        System.out.println(VM.current().addressOf(s));
+        s = s + " Javooni";
+        System.out.println(VM.current().addressOf(s));
+
+        StringBuffer sb = new StringBuffer("Hossien");
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+    }
+
+}
