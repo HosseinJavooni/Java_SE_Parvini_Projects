047e13cfc08c3a07ff9fad516c3797a4b2b49f09
HosseinJavooni <h.javooni@gmail.com>
2023-05-22 16:45:30 +0330

    1402_03_01_1645pm

---
+++ Session_06/src/main/java/QueryBuilding_MethodChaining_or_MethodCascading_Sample/MainOfQueryBuilder.java
@@ -0,0 +1,12 @@
+package QueryBuilding_MethodChaining_or_MethodCascading_Sample;
+
+public class MainOfQueryBuilder {
+    public static void main(String[] args){
+        QueryBuilder query = new QueryBuilder();
+        query
+                .select("id, first_name, last_name")
+                .from("Customer")
+                .where("age <= 25")
+                .print();
+    }
+}
